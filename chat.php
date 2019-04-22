e4a01859a7a06667a706feeccc648672d1905a6d
Ankushbruno <36437792+Ankushbruno@users.noreply.github.com>
2019-04-23 01:57:12 +0530

    Add files via upload

---
+++ chat.php
@@ -0,0 +1,19 @@
+
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>chatbot></title>
+	<link rel="stylesheet" type="text/css" href="css/chatstyle.css">
+
+</head>
+<body>
+	<div class="chatbox">
+	</div>
+        <br>
+	<input type="text" id="input" placeholder="Type Your message..." style="margin-left: 350px;width: 535px;border-radius: 5px; height: 30px;">
+	<button class="button" type="button" onclick="Send()" style=" height: 30px;margin-left: 3px;position: absolute;margin-top: 0px;">send</button>
+	
+	<script type="text/javascript" src="jquery/jquery.js"></script>
+	<script src="js/chatcode.js"></script>
+</body>
+</html>
